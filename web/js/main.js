861bf3af6ee0e39631c98afbe7dab4a317e0c1be
手嶋守 <tejima@gmail.com>
2012-01-25 20:13:33 +0900

    ヘッダーを消す。
    ドロップダウンの研究

---
+++ web/js/main.js
@@ -0,0 +1,10 @@
+//
+
+$(document).ready(function(){
+  $("#Header").hide();
+  $(".input_submit").wrap('<div class="row"><div class="span6 offset3"></div></div>');
+  $(".input_submit").addClass("btn primary span6 small");
+  $("#Top").addClass("span8");
+  $("#MailAddressLogin th").addClass("span5");
+  $("#MailAddressLogin label").addClass("span5");
+});
